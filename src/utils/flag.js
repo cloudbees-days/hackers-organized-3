c701493745cb185586b645e1b74e51d2aae567fe
Logan Donley <ldonley@cloudbees.com>
2024-11-05 13:20:37 -0500

    cleaning up some junk

--- src/utils/flag.js
+++ src/utils/flag.js
@@ -16,37 +16,9 @@ export const impressionHandler = (reporting) => {
   }
 }
 
-//const API_HOST = 'https://api.cloudbees.io'
-//const API_HOST = 'https://api-staging.saas-dev.beescloud.com'
 const options = {
   configurationFetchedHandler: configurationFetchedHandler,
-  impressionHandler: impressionHandler/*,
-  configuration: {
-    CD_API_ENDPOINT: 'https://api.saas-preprod.beescloud.com/device/get_configuration',
-    CD_S3_ENDPOINT: 'https://rox-conf.saas-preprod.beescloud.com/',
-    SS_API_ENDPOINT: 'https://api.saas-preprod.beescloud.com/device/update_state_store/',
-    SS_S3_ENDPOINT: 'https://development-statestore.rollout.io/',
-    CLIENT_DATA_CACHE_KEY: 'client_data',
-    ANALYTICS_ENDPOINT: 'https://localhost:8787',
-    NOTIFICATIONS_ENDPOINT: 'https://api.saas-preprod.beescloud.com/sse'
-  },
-  debugLevel: 'verbose',
-  disableSignatureVerification: true
-  */
-    
-  /*,
-  configuration: {
-    API_HOST: API_HOST,
-    CD_API_ENDPOINT: `${API_HOST}/device/get_configuration`,
-    CD_S3_ENDPOINT: 'https://development-conf.rollout.io/',
-    SS_API_ENDPOINT: `${API_HOST}/device/update_state_store/`,
-    SS_S3_ENDPOINT: 'https://development-statestore.rollout.io/',
-    CLIENT_DATA_CACHE_KEY: 'client_data',
-    ANALYTICS_ENDPOINT: 'https://localhost:8787',
-    NOTIFICATIONS_ENDPOINT: 'https://api-staging.saas-dev.beescloud.com/sse'
-  },
-  debugLevel: 'verbose',
-  disableSignatureVerification: true*/
+  impressionHandler: impressionHandler
 }
 
 export const Flags = {
@@ -54,14 +26,12 @@ export const Flags = {
   ask: new Rox.Flag(false),
   show: new Rox.Flag(false),
   headerColor: new Rox.RoxString('is-dark', ['is-dark', 'is-primary', 'is-white'])
-  
+
 }
 
 Rox.setCustomBooleanProperty('isBetaUser', betaAccess())
 Rox.setCustomBooleanProperty('isLoggedIn', isLoggedIn())
 Rox.setCustomStringProperty('company', getCompany())
-//Rox.setCustomStringProperty('getGregProperty', getCompany())
 
 Rox.register('default', Flags)
-Rox.setup('7187091f-0ce5-40a6-48f0-d1d8e82fc1ef', options)
-
+Rox.setup(process.env.VUE_APP_ROLLOUT_KEY, options)
