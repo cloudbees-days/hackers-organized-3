43f551c6d4f9135f3a4656ff232ac8cc101d4bec
Logan Donley <ldonley@cloudbees.com>
2024-11-06 09:58:24 -0500

    switching to sha for img

--- tests/unit/ListItem.spec.js
+++ tests/unit/ListItem.spec.js
@@ -1,19 +1,6 @@
-import { shallowMount } from '@vue/test-utils'
-import ListItem from '@/components/ListItem.vue'
+import { render, fireEvent, screen } from "@testing-library/vue";
+import ListItem from "@/components/ListItem.vue";
 
-describe('ListItem.vue', () => {
-  it('renders props.title when passed', async () => {
-    const title = 'Super popular article'
-    const wrapper = shallowMount(ListItem, {
-      propsData: { title }
-    })
-    expect(wrapper.text()).toMatch(title)
-  })
-  it('renders props.user when passed', async () => {
-    const user = 'testuser'
-    const wrapper = shallowMount(ListItem, {
-      propsData: { user }
-    })
-    expect(wrapper.text()).toMatch(user)
-  })
-})
+test("home page loads", async () => {
+  render(ListItem);
+});
